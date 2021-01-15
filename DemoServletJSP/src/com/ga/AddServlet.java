832ea1f218845127d9ddab1ff82fd315d5fd0a83
johnfa9 <jacocella@hotmail.com>
2021-01-15 14:45:40 -0500

    completed project

---
+++ DemoServletJSP/src/com/ga/AddServlet.java
@@ -0,0 +1,54 @@
+package com.ga;
+
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//public class AddServlet extends HttpServlet{
+
+//	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		
+//		int k = i + j;
+//		
+//		PrintWriter out = res.getWriter();
+//		
+//		out.println("result is " + k);
+//	}
+	
+	
+	//instead of service method use doPost or doGet to limit the type of request the servlet accepts
+//	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		
+//		int k = i + j;
+//		
+//		PrintWriter out = res.getWriter();
+//		
+//		out.println("result is " + k);
+//	}
+	
+//	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+//		int i = Integer.parseInt(req.getParameter("num1"));
+//		int j = Integer.parseInt(req.getParameter("num2"));
+//		
+//		int k = i + j;
+//		
+//		
+//		
+//		PrintWriter out = res.getWriter();
+//		out.println("<html><body bgcolor='cyan'>");
+//		out.println("Output : " + k);
+//		out.print("</body></html>");
+		
+//		
+//	}
+//
+//}
