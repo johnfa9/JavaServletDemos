2f07ce320fd850d66b425d6f6fe43c698d66b1dd
johnfa9 <jacocella@hotmail.com>
2021-01-15 14:46:25 -0500

    completed project

---
+++ DemoServletLogin/src/com/ga/Login.java
@@ -0,0 +1,35 @@
+package com.ga;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uname = request.getParameter("uname");
+		String pass = request.getParameter("pass");
+		//could check against a password here
+		if(uname.equals("navin") && pass.equals("123")){
+			HttpSession session = request.getSession();
+			session.setAttribute("username", uname);
+			response.sendRedirect("welcome.jsp");
+		}
+		else {
+			response.sendRedirect("login.jsp");
+		}
+		
+	}
+
+
+}
